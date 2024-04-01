7da83513804fb2b889d8495bb73e2d8433e01a28
vinitakoranga <156450124+vinitakoranga@users.noreply.github.com>
2024-04-01 09:20:47 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+let loginBtn=document.querySelector('.btn')
+let remeberBtn=document.querySelector('.remember-forget')
+let registerBtn=document.querySelector('.register-link')
+let userBtn=document.querySelector('.input-box')
+
+
+loginBtn.addEventListener('click',()=>
+{
+    userBtn.style.maxHeight='0';
+})
